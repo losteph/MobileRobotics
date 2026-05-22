728f3c65d27a49b7d604bc94d1a4625982822517
Stefano Di Lena <stephdln02@gmail.com>
2026-05-22 19:22:06 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+xhost +
+docker run -it --rm --net host --ipc host --privileged \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ~/.Xauthority:/root/.Xauthority \
+    -e DISPLAY=$DISPLAY \
+    -e XAUTHORITY=$XAUTHORITY \
+    -v "$(pwd)/ros_ws/:/root/ros_workspace" \
+    --name project_container \
+    ros:project bash
