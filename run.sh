d3a27fb8f2bb32e9b19e6e9e32df6743b611400e
Stefano Di Lena <stephdln02@gmail.com>
2026-06-24 16:28:36 +0200

    Comment out GPU options in run.sh
    Comment out GPU-related Docker options in run.sh

--- run.sh
+++ run.sh
@@ -6,9 +6,6 @@ docker run -it --rm --net host --ipc host --privileged \
     -v ~/.Xauthority:/root/.Xauthority \
     -e DISPLAY=$DISPLAY \
     -e XAUTHORITY=$XAUTHORITY \
-    #--gpus all \ # scommentare se avete una scheda grafica esterna
-    #--env="NVIDIA_DRIVER_CAPABILITIES=all" \ # e non la legge
-    #--env="QT_X11_NO_MITSHM=1" \
     -v "$(pwd)/ros_ws/:/root/ros_workspace" \
     --name project_container \
     ros:project bash
