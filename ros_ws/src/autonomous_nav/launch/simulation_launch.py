e20ecfc876356c75a1aca8f8a6953f2bbb41399b
Stefano Di Lena <stephdln02@gmail.com>
2026-06-26 11:25:38 +0200

    Refactor simulation launch file for clarity and structure

--- ros_ws/src/autonomous_nav/launch/simulation_launch.py
+++ ros_ws/src/autonomous_nav/launch/simulation_launch.py
@@ -10,14 +10,13 @@ from launch_ros.parameter_descriptions import ParameterValue
 def generate_launch_description():
     pkg_autonomous_nav = get_package_share_directory('autonomous_nav')
 
-    # 1. Percorsi dei file
+    # Percorsi dei file
     world_file = os.path.join(pkg_autonomous_nav, 'worlds', 'track.sdf')
     urdf_file = os.path.join(pkg_autonomous_nav, 'urdf', 'robot.urdf.xacro')
 
-    # Usa xacro per convertire il file in stringa XML
-    robot_desc = ParameterValue(Command(['xacro ', urdf_file]), value_type=str)
+    robot_desc = ParameterValue(Command(['xacro ', urdf_file]), value_type=str)     # (xacro per convertire il file in stringa XML)
 
-    # 2. Avvia Gazebo con la mappa
+    # Avviamo Gazebo con la mappa
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
@@ -25,21 +24,21 @@ def generate_launch_description():
         launch_arguments={'gz_args': f'-r {world_file}'}.items()
     )
 
-    # 3. Nodo Robot State Publisher (Serve a RViz per capire la forma del robot)
+    # Nodo Robot State Publisher (Serve a RViz per capire la forma del robot)
     rsp_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': robot_desc}]
     )
 
-    # 4. Inserisce il robot in Gazebo (Legge dal topic ufficiale)
+    # Spawn del robot in Gazebo (Legge dal topic ufficiale)
     spawn_node = Node(
         package='ros_gz_sim',
         executable='create',
         arguments=['-topic', 'robot_description', '-name', 'my_custom_bot', '-x', '2.0', '-y', '0.75', '-z', '0.2'],
         output='screen'
     )
-    # 5. Il Ponte Magico (Fa comunicare i topic di ROS 2 con Gazebo)
+    # Ponte  (comunicazione tra i topic di ros2 e Gazebo)
     bridge_node = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
